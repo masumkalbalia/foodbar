8a8a08b9044d5a5bc770a15d575f94cd5c4e21f8
masumkalbalia <166114142+masumkalbalia@users.noreply.github.com>
2024-11-25 14:37:11 +0530

    Add files via upload

---
+++ project 4/script.js
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="cursor"></div>
+    <div class="main">
+        <div class="page1">
+            <div id="nav">
+                <img src="image/logo.png" alt="">
+                <div id="nav-part2">
+                    <h4>Home</h4>
+                    <h4>Work</h4>
+                    <h4>Studio</h4>
+                    <h4>Contact</h4>
+                </div>
+                <div id="nav-part3">
+                    <div id="circle">
+                        <!-- <h2>prince</h2> -->
+                    </div>
+                </div>
+            </div>
+            <h1>Digitally crafted</h1>
+            <h2>brand experiences</h2>
+            <video autoplay muted loop src="vedio/video.mp4"></video>
+        </div>
+        <div class="page2">
+            <h1>WE are Duo Studio,</h1>
+            <div class="page2-container">
+                <div class="page2-left">
+                    <h2> A CREATIVE 
+                        COLLECTIVE
+                        MADE TO UNLOCK
+                        YOUR BRAND'S
+                        POTENTIAL
+                    </h2>
+                </div>
+                <div class="page2-right">
+                    <P>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat eos consectetur, nobis nesciunt illo.</P>
+                    <button>About us</button>
+                </div>
+            </div>
+        </div>
+        <div class="page4">
+            <div class="elem">
+                <img src="image/prince.jpg" alt="">
+                <div class="text-div">
+                    <h1>PRINCE</h1>
+                    <h1>PRINCE</h1>
+                </div>
+                <img src="image/prince.jpg" alt="">
+            </div>
+            <div class="elem">
+                <img src="image/premkumarsingh.jpg" alt="">
+                <div class="text-div">
+                    <h1>PREM</h1>
+                    <h1>PREM</h1>
+                </div>
+                <img src="image/premkumarsingh.jpg" alt="">
+            </div>
+            <div class="elem">
+                <img src="image/shafar.jpg" alt="">
+                <div class="text-div">
+                    <h1>SHAFAR</h1>
+                    <h1>SHAFAR</h1>
+                </div>
+                <img src="image/shafar.jpg" alt="">
+            </div>
+        </div>
+        <div class="page5">
+            <h2>Mentions Clients</h2>
+            <div class="box" data-image="image/premkumarsingh.jpg">
+                <h3>prem</h3>
+                <h4>2024</h4>
+            </div>
+            <div class="box" data-image="image/premkumarsingh.jpg">
+                <h3>shafar</h3>
+                <h4>2024</h4>
+            </div>
+            <div class="box" data-image="image/premkumarsingh.jpg">
+                <h3>prince</h3>
+                <h4>2024</h4>
+            </div>
+            <div class="box" data-image="">
+                <h3>rahul</h3>
+                <h4>2024</h4>
+            </div>
+            <footer>
+                
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="script.js"> </script>
+</body>
+</html>
